2383ca700981e82e2c4db80ff88184e6ad2dadd9
guojiajia <2968180168@qq.com>
2020-04-04 12:30:06 +0800

    在"dev-gjj"上提交

--- src/pages/usedMarket/news/index.vue
+++ src/pages/usedMarket/news/index.vue
@@ -1,6 +1,81 @@
 <template>
   <div>
-     <p>进入二手交易市场的消息页面</p>
+    <div class="whole_back">
+    <div class="head_back"></div>
+     <div class="head">
+        <div class="headContent">
+          <img src="../../../../static/images/newsAndperson/icon/icon_system_top.png">
+          <p>通知消息</p>
+        </div>
+        <div class="headContent">
+          <img src="../../../../static/images/newsAndperson/icon/icon_reply_message_entry.png">
+          <p>互动消息</p>
+        </div>
+        <div class="headContent">
+          <img src="../../../../static/images/newsAndperson/icon/icon_sell_top.png">
+          <p>活动消息</p>
+        </div>
+        <div class="headContent">
+          <img src="../../../../static/images/newsAndperson/icon/icon_pond_top.png">
+          <p>鱼塘消息</p>
+        </div>
+     </div>
+
+     <div class="advertise">   
+       <img src="../../../../static/images/newsAndperson/advertise/operation_enter.png">   
+     </div>
+     
+
+       <div class="newsList">
+         <img class="imgFl" src="../../../../static/images/newsAndperson/avater/1.png">
+
+         <div class="newsContent">
+           <div class="text">
+           <h2><strong>syj1138962153</strong></h2>
+           <p>[呲牙][呲牙]</p>
+           <span>1天前</span>
+           </div>
+           <img src="../../../../static/images/newsAndperson/goods/1.png">     
+         </div>    
+       </div>
+
+       <div class="newsList">
+        <img class="imgFl" src="../../../../static/images/newsAndperson/avater/2.png">
+        <div class="newsContent">
+          <div class="text">
+          <h2><strong>生如夏花</strong></h2>
+          <p>你有一条新消息</p>
+          <span>1天前</span>
+          </div>
+        <img src="../../../../static/images/newsAndperson/goods/2.png">     
+        </div>    
+      </div>
+
+      <div class="newsList">
+        <img class="imgFl" src="../../../../static/images/newsAndperson/avater/3.png">
+        <div class="newsContent">
+          <div class="text">
+          <h2><strong>幸福树168</strong></h2>
+          <p>你有一条新消息</p>
+          <span>2天前</span>
+          </div>
+        <img src="../../../../static/images/newsAndperson/goods/3.png">     
+        </div>    
+      </div>
+
+      <div class="newsList">
+        <img class="imgFl" src="../../../../static/images/newsAndperson/avater/4.png">
+        <div class="newsContent">
+          <div class="text">
+          <h2><strong>sin520</strong></h2>
+          <p>是呀，他这个对话框有的会多一些</p>
+          <span>2天前</span>
+          </div>
+        <img src="../../../../static/images/newsAndperson/goods/4.png">     
+        </div>    
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -18,5 +93,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "./style.styl";
 </style>
+
--- src/pages/usedMarket/person/index.vue
+++ src/pages/usedMarket/person/index.vue
@@ -1,6 +1,102 @@
 <template>
-  <div>
-     <p>进入二手交易市场的个人中心</p>
+  
+  <div class="back">
+  <div class="header">
+    <img src="../../../../static/images/newsAndperson/avater/default_user_avatar.png">
+    <div class="headerContent">
+      <h4>t_147952436838413_0</h4>
+      <p>昵称：未设置</p>
+      <span> 个人主页> </span>
+    </div>
+  </div>
+
+  <div class="fans">
+    <p>0超赞</p>
+    <p>0关注</p>
+    <p class="none">0粉丝</p>
+  </div>
+
+  <div class="advertise"> 
+    <img src="../../../../static/images/newsAndperson/advertise/advertise.png">
+  </div>
+
+  <div class="part1"> 
+    <h2>卖在闲鱼</h2>
+    <div class="sell">
+      <div class="sellThings">
+        <img src="../../../../static/images/newsAndperson/small_icon/sell_1.PNG">
+        <p>我发布的0</p>
+      </div>
+      <div class="sellThings">
+        <img src="../../../../static/images/newsAndperson/small_icon/sell_2.PNG">
+        <p>我卖出的0</p>
+      </div>
+      <div class="sellThings">
+        <img>
+        <p></p>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="part1"> 
+    <h2>买在闲鱼</h2>
+    <div class="sell">
+      <div class="sellThings">
+        <img src="../../../../static/images/newsAndperson/small_icon/buy_1.PNG">
+        <p>我买到的0</p>
+      </div>
+      <div class="sellThings">
+        <img src="../../../../static/images/newsAndperson/small_icon/buy_2.PNG">
+        <p>我收藏的0</p>
+      </div>
+      <div class="sellThings">
+        <img src="../../../../static/images/newsAndperson/small_icon/buy_3.PNG">
+        <p>我租到的</p>
+      </div>
+    </div>
+    
+  </div>
+
+  <div class="part1"> 
+    <h2>玩在闲鱼</h2>
+    <div class="sell">
+      <div class="sellThings">
+        <img src="../../../../static/images/newsAndperson/small_icon/play_1.PNG">
+        <p>闲鱼币</p>
+      </div>
+      <div class="sellThings">
+        <img src="../../../../static/images/newsAndperson/small_icon/play_2.PNG">
+        <p>我的鱼塘</p>
+      </div>
+      <div class="sellThings">
+        <img src="../../../../static/images/newsAndperson/small_icon/play_3.PNG">
+        <p>我的帖子</p>
+      </div>
+      <div class="sellThings">
+        <img src="../../../../static/images/newsAndperson/small_icon/play_4.PNG">
+        <p>边逛边赚钱</p>
+      </div>
+      <div class="sellThings">
+        <img src="../../../../static/images/newsAndperson/small_icon/play_5.PNG">
+        <p>天天赚钱</p>
+      </div>
+      <div class="sellThings">
+        <img src="../../../../static/images/newsAndperson/small_icon/play_6.PNG">
+        <p>百币夺宝</p>
+      </div>
+      <div class="sellThings">
+        <img src="../../../../static/images/newsAndperson/small_icon/play_7.PNG">
+        <p>参与的免费送</p>
+      </div>
+      <div class="sellThings">
+        <img src="">
+        <p></p>
+      </div>
+    </div>
+
+</div>
+
   </div>
 </template>
 
@@ -18,5 +114,6 @@ export default {
 }
 </script>
 
-<style>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "./style.styl";
 </style>
