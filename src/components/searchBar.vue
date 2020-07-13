e252c6cf18d502809ac3f2e695497d5dee7a2f3e
guojiajia <2968180168@qq.com>
2020-07-13 09:12:19 +0800

    搜索和评论功能

--- src/components/searchBar.vue
+++ src/components/searchBar.vue
@@ -2,19 +2,30 @@
 <template>
   <div class="search">
     <div class="search_goods">
-      <input type="text" placeholder="搜索">
+      <input type="text" v-model="words" placeholder="搜索" maxlength="15"
+      confirm-type="search" focus="true" @input="search" @confirm="searchWords">
       <span class="icon"></span>
     </div>
   </div>
 </template>
 
 <script>
+  import {API} from "@/api/api";
   export default {
     props: {},
     data() {
-      return {}
+      return {
+        words:"",
+      }
     },
-    methods: {}
+    methods: {
+      search(){//键盘输入时触发
+        this.words=this.words.replace(/[^\w\u4E00-\u9FA5\s*]/g, '');
+      },
+      searchWords(){//点击完成按钮时触发
+        
+      },
+    }
   }
 
 </script>
